1c987298c908bba47358536da725df2314b1eb1d
AdrianMorenoNystrom <adrianmorenonystrom@gmail.com>
2023-09-06 16:58:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Siffra = 12;
+
+        }
+    }
+}
