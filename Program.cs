797532cc6cd476c215f2b90dc3c30845b50f3ec7
AdrianMorenoNystrom <adrianmorenonystrom@gmail.com>
2023-09-06 18:39:56 +0200

    HEJ

--- Program.cs
+++ Program.cs
@@ -6,8 +6,25 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            int Siffra = 12;
+            Console.WriteLine("Skriv in ett tal");
+            string input = Console.ReadLine();
+            int siffra;
+            Int32.TryParse(input, out siffra);
 
+            if (siffra >= 10)
+            {
+                Console.WriteLine("Talet är stort!");
+            }
+            else
+            {
+                Console.WriteLine("Oj. Lågt tal!");
+            }
+
+            Console.WriteLine("Vad heter du?");
+            string namn = Console.ReadLine();
+            Console.WriteLine($"Hej, { namn}!");
+
+            // HÄR SKA DET VARA EN LOOP som skriver ut alla tal från noll upp till det nummer som du satte i din tidigare variabel.
         }
     }
 }
